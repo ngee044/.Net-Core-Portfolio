c6ae3674875c8cfd2f0646e472322ff7463a705b
ngee044 <mgee044@naver.com>
2021-03-16 00:44:13 +0900

    StoreageSpace Class 추가 및 NuGet 패키지 추가 -.Engine에 ValueTuple 패키지 추가 및 사용 -확장 메서드 클래스를 정적으로 선언

--- EbookManagerApp/eBookManager.Engine/DocumentEngine.cs
+++ EbookManagerApp/eBookManager.Engine/DocumentEngine.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using static System.ValueTuple;
 
 namespace eBookManager.Engine
 {
     class DocumentEngine
     {
+        public (DateTime dateCreated, DateTime dateLastAccessed, string fileName, string fileExtension, long fileLength, bool error)
+            GetFileProperites (string filePath)
+        {
+            //tuple 초기화
+            var returnTuple = (created: DateTime.MinValue,
+                lastDateAccessed: DateTime.MinValue, name: "", ext: "", fileSize: 0L, error: false);
+
+            try
+            {
+                FileInfo fi = new FileInfo(filePath);
+                fi.Refresh();
+                returnTuple = (fi.CreationTime, fi.LastAccessTime, fi.Name, fi.Extension, fi.Length, false);
+            }
+            catch
+            {
+                returnTuple.error = true;
+            }
+            return returnTuple;
+        }
     }
 }
--- EbookManagerApp/eBookManager.Helper/ExtensionMethods.cs
+++ EbookManagerApp/eBookManager.Helper/ExtensionMethods.cs
@@ -1,12 +1,95 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eBookManager.Engine;
+using Newtonsoft.Json;
 
 namespace eBookManager.Helper
 {
-    public class ExtensionMethods
+    public static class ExtensionMethods
     {
+        public static int ToInt(this string value, int defaultInteger = 0)
+        {
+            try
+            {
+                if(int.TryParse(value, out int validInterger))
+                {
+                    return validInterger;
+                }
+                else
+                {
+                    return defaultInteger;
+                }
+            }
+            catch
+            {
+                return defaultInteger;
+            }
+        }
+
+        public static double ToMegabytes(this long bytes)
+        {
+            return bytes > 0 ? (bytes / 1024f) / 1024f : bytes;
+        }
+
+        public static bool StorageSpaceExists (this List<StorageSpace> space, string nameValueToCheck, out int storageSpaceId)
+        {
+            bool exists = false;
+            storageSpaceId = 0;
+
+            if(space.Count() != 0)
+            {
+                int count = (from r in space where r.Name.Equals (nameValueToCheck) select r).Count();
+
+                if(count > 0)
+                {
+                    exists = true;
+                }
+
+                storageSpaceId = (from r in space select r.ID).Max() +1; // 새로운 공간 추출
+            }
+            return exists;
+        }
+
+        public static void WriteToDataStore(this List<StorageSpace> value, string storagePath, bool appendToExistingFile = false)
+        {
+            JsonSerializer json = new JsonSerializer();
+            json.Formatting = Formatting.Indented;
+            using (StreamWriter sw = new StreamWriter(storagePath, appendToExistingFile))
+            {
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                {
+                    json.Serialize(writer, value);
+                }
+            }
+        }
+
+        public static List<StorageSpace> ReadFromDataStore (this List<StorageSpace> value, string storagePath)
+        {
+            JsonSerializer json = new JsonSerializer();
+            if(!File.Exists(storagePath))
+            {
+                var newFile = File.Create(storagePath);
+                newFile.Close();
+            }
+            using (StreamReader sr = new StreamReader(storagePath))
+            {
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    var retVal = json.Deserialize<List<StorageSpace>>(reader);
+                    if(retVal is null)
+                    {
+                        retVal = new List<StorageSpace>();
+
+                        return retVal;
+                    }
+                }
+            }
+            return value;
+        }
+
     }
 }
